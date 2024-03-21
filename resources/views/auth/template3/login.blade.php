dcce7b88235df0f391642d48e82565e3c81303ba
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2024-03-21 23:41:00 +0500

    asdasd

--- resources/views/auth/template3/login.blade.php
+++ resources/views/auth/template3/login.blade.php
@@ -423,10 +423,10 @@
             containerr.classList.remove('right-panel-active'));
     </script>
 
-    <script type="text/javascript" src="https://d3a39i8rhcsf8w.cloudfront.net/js/jquery.mask.min.js"></script>
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $('#dateofbirthfull').mask('00/00/0000');
-        });
-    </script>
+    <script type="text/javascript" src="{{ url('public/front/js/jquery.mask.min.js') }}"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#dateofbirthfull').mask('00/00/0000');
+    });
+</script>
 @endsection
