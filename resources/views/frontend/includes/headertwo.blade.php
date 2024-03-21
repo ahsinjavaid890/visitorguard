ad978b184e7e27470a19c93d24e9abbb46e8008e
Ahsin Javaid <52376720+ahsinjavaid890@users.noreply.github.com>
2024-03-21 22:59:58 +0500

    Update headertwo.blade.php

--- resources/views/frontend/includes/headertwo.blade.php
+++ resources/views/frontend/includes/headertwo.blade.php
@@ -1,6 +1,10 @@
+@php
+    $url = request()->segment(count(request()->segments()));
+    $settings = DB::table('site_settings')->where('smallname' , 'visitorguard')->first();
+@endphp
 <section class="tabshead">
 	<div class="row" style="padding: 15px; margin:0;">
-	<div class="col-md-6 col-xs-6"><a href="{{ url('') }}"><img src="{{ url('public/front/visitorguard/images/logo-white.png') }}" /></a></div>
+	<div class="col-md-6 col-xs-6"><a style="width: 280px" href="{{ url('') }}"><img src="{{ url('public/images') }}/{{ $settings->header_logo }}" /></a></div>
 	<div class="col-md-6 col-xs-6 text-right" style="padding-top: 20px;"><a href="tel:8555008999" style="text-decoration:none; color:#FFF; font-size:26px;"><i class="fa fa-phone"></i> <span class="hidden-xs">855-500-8999</span></a></div>
 	</div>
 </section>
